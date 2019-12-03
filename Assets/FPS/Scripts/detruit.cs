022f84d3dc9b86d157eb797680d3c9d9596a1f33
charli <charli.pacquet@gmail.com>
2019-12-03 14:27:32 +0100

    clef plus porte

---
+++ Assets/FPS/Scripts/detruit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detruit : MonoBehaviour
+{
+    public GameObject door;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+            Destroy(door);  
+        }
+    }
+    
+}
