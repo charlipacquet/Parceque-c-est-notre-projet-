e3d4c370a2ffc4b3f4b744aa01e4db1d85ba35a8
charli <charli.pacquet@gmail.com>
2019-12-03 17:12:43 +0100

    fin du jeu

---
+++ Assets/FPS/Scripts/armededieu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class armededieu : MonoBehaviour
+{
+    public GameObject piececaché;
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Y))
+        {
+            Destroy(piececaché);
+        }
+    }
+}
